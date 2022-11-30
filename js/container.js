7f70d102fb0ae0b00fb0ea9250245e74e5014cf7
Serhii Myshko <sergeiown@gmail.com>
2022-11-30 21:23:00 +0200

    add reversNumber func

--- js/container.js
+++ js/container.js
@@ -1,1 +1,19 @@
-console.log(123 % 10);
+reversedNumber = () => {
+  let userNunber = 123456789;
+
+  if (userNunber > 0) {
+    let lastNunber = "";
+    var resultNumber = "";
+
+    while (userNunber > 0) {
+      lastNunber = userNunber % 10;
+      userNunber = Math.floor(userNunber / 10);
+      resultNumber += lastNunber;
+    }
+  } else {
+    console.log("Is not valid nmber");
+  }
+  return resultNumber;
+};
+
+console.log(reversedNumber());
