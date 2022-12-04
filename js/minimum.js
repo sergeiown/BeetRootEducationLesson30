d1d2d33a0763ea813f34a057430f2eb9e5177fd5
Serhii Myshko <sergeiown@gmail.com>
2022-12-04 12:03:55 +0200

    add files quantity

--- js/minimum.js
+++ js/minimum.js
@@ -11,10 +11,23 @@ let addition = (firstNumber, secondNumber) => {
   }
 };
 
+let flashDriveCapacity = (capacity) => {
+  const invalidNumber =
+    "not calculated because flash drive capacity is not chosen.";
+
+  if (capacity > 0) {
+    const resultNumber = Math.round((capacity * 1024) / 850);
+
+    return resultNumber;
+  } else {
+    return invalidNumber;
+  }
+};
+
 //The following task is not informative enough to build the function:
 // let first = "1";
 // let second = 2;
 // let result = Number(first) + Number(second);
 // console.log(result);
 
-export { addition };
+export { addition, flashDriveCapacity };
